49b163240a6ee89bbaf045f70e4900c051646145
wael kamira <ramondm4@gmail.com>
2023-04-16 17:36:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    const left = parseInt(dodger.style.left.replace("px", ""));
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const left = parseInt(dodger.style.left.replace("px", ""));
+   
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
